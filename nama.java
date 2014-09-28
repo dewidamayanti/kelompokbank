da2d58b7fbf4c83eec2c53bbc952d5b6cc6e8bf3
dewidamayanti <wiedamay94@gmail.com>
2014-09-28 22:29:31 +0700

    commit1

---
+++ nama.java
@@ -0,0 +1,10 @@
+public class nama {
+      public static void main(String[] args) {
+	  
+	  System.out.println("Nama Kelompok Tabungan SA");
+	  System.out.println("- Dewi Damayanti (1127050037)");
+	  System.out.println("- Yana Tera(1127050170");
+	  
+	  }
+}
+	  
